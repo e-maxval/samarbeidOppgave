431f0ac854468b89502e1a2ee527373e59b0d17a
e-maxval <maxieier10@gmail.com>
2022-12-09 09:13:18 +0100

    lagde class

--- oppgave1.py
+++ oppgave1.py
@@ -4,29 +4,51 @@ from pathlib import Path
 import csv
 
 fil = Path(__file__).parent / "Befolkning.csv"
-xverdier = []
-yverdier = []
 
-with open(fil, encoding = "utf-8-sig") as f:
-    i = 0
-    next(f)
-    next(f)
-    filInnhold = csv.reader(f, delimiter=";")
-    overskrifter = next(filInnhold)
-    for rad in filInnhold:
-        xverdier.append(int(rad[0]))
-        yverdier.append(int(rad[1]))
-        i += 1
+class csvTilgraf:
+    def __init__(self, filnavn:str, tegnsett:str = "utf-8-sig"):
+        self.fil = Path(__file__).parent / filnavn
+        self.xverdier = []
+        self.yverdier = []
+        self.tegnsett = tegnsett
+        self.overskrifter = []
+        self.tittel = ""
+    
+    def klargjør(self):
+        with open(self.fil, encoding= self.tegnsett) as f:
+            self.tittel = f.readlines(1)
+            for line in f:
+                line = line.rstrip().split(";")
+                print(line)
+                if len(line) == 2: 
+                    self.overskrifter = line
+                    self.overskrifter = [i.replace('"','') for i in self.overskrifter]
+                    print(self.overskrifter)
+                    break
 
-xdiff = (abs(max(xverdier)-min(xverdier)))/25
-ydiff = (abs(max(yverdier)-min(yverdier)))/25
+            filInnhold = csv.reader(f, delimiter=";")
+            for rad in filInnhold:
+                self.xverdier.append(int(rad[0]))
+                self.yverdier.append(int(rad[1]))
 
-plt.plot(xverdier, yverdier, color = 'blue', linestyle = '--')
-plt.title("Folketall fra 1769")      # tittel
-plt.xlabel("År")             # x-aksetittel
-plt.ylabel("Befolkning")     # y-aksetittel
-plt.xlim(min(xverdier) - xdiff,max(xverdier) + xdiff)   # definisjonsmengde
-plt.ylim(min(yverdier) - ydiff,max(yverdier) + ydiff)   # verdimengde
-plt.grid()       # tegner rutenett
-plt.legend()
-plt.show()
+    def lagGraf(self):
+        xdiff = (abs(max(self.xverdier)-min(self.xverdier)))/25
+        ydiff = (abs(max(self.yverdier)-min(self.yverdier)))/25
+
+        plt.plot(self.xverdier, self.yverdier, color = 'blue', linestyle = '--')
+        plt.title(self.tittel)     # tittel
+        plt.xlabel(self.overskrifter[0])     # x-aksetittel
+        plt.ylabel(self.overskrifter[1])     # y-aksetittel
+        plt.xlim(min(self.xverdier) - xdiff,max(self.xverdier) + xdiff)   # definisjonsmengde
+        plt.ylim(min(self.yverdier) - ydiff,max(self.yverdier) + ydiff)   # verdimengde
+        plt.grid()       # tegner rutenett
+        plt.legend()
+
+    def tegnGraf(self):
+        self.lagGraf()
+        plt.show()
+        
+
+graf = csvTilgraf("Befolkning.csv")
+graf.klargjør()
+graf.tegnGraf()
